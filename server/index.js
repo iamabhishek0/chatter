0cbef62d031e5f6192c19297ab155bf3b3640f16
Abhishek Chaurasia <abhi24061997@gmail.com>
2021-08-31 12:09:26 +0530

    Socket Connection Set

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+// const io = socketio(server);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+	},
+});
+app.use(cors());
+app.use(router);
+
+io.on("connect", (socket) => {
+	console.log("We have a new connection!!");
+
+	socket.on("disconnect", () => {
+		console.log("User has left!!");
+	});
+});
+
+server.listen(PORT, () => console.log(`Server has started on port : ${PORT}`));
